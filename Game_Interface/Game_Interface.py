557ef48f224a8d28c94f15ce9ab6147b34d4604f
kkusashio <kkusashio@gmail.com>
2021-10-01 19:58:03 +0900

    valid or invalid input

--- Game_Interface/Game_Interface.py
+++ Game_Interface/Game_Interface.py
@@ -8,7 +8,9 @@ import random
 
 number = []
 numberchoice = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+numberchoices = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
 attempts = 0
+gamewon=False
 
 def GenerateNum():
     for i in range(5):
@@ -16,7 +18,7 @@ def GenerateNum():
         y = numberchoice[x]
         del numberchoice[x] #delete number used to prevent overlap
         number.append(y) # add elements to the list
-    #print(number)
+    print(number)
 
 def TestFunc():
     print ("Answer is", number)
@@ -24,12 +26,37 @@ def TestFunc():
 def GameEnd():
     print("GAME WON", " Attempts: ", attempts)
 
+def get_unique_list(seq):
+    seen = []
+    return [x for x in seq if x not in seen and not seen.append(x)]
+
 def Game():
     global attempts
     attempts+=1
     blow=0
     hit=0
-    choice = input("0~fを5桁入力してください： ")
+    valid_num=False
+    while (valid_num == False):
+        valid_input_char=False
+        invalid_input_len=False  
+        invalid_input_same=False
+        choice = input("0~fを5桁入力してください： ")
+        if (len(choice)!=5):
+            invalid_input_len=True
+        for i in range(len(choice)):
+            for j in range(len(numberchoices)):
+                if choice[i]==numberchoices[j]:
+                    valid_input_char=True
+        if (len(choice)!=len(set(choice))):
+            invalid_input_same=True
+        if valid_input_char==False:
+            print("Invalid character, try again")
+        if invalid_input_len==True:
+            print("Invalid digits, try again")  
+        if invalid_input_same==True:
+            print("Same character cannot be used twice, try again")   
+        else:
+            valid_num=True
     guess = []
     for i in range(5):
         guess.append(choice[i])
@@ -42,6 +69,7 @@ def Game():
     
     #check if game won
     if(hit==5):
+        gamewon=True
         GameEnd()
     
     #print result
@@ -51,5 +79,6 @@ def Game():
 #Functions
 GenerateNum()
 TestFunc()
-Game()
+while gamewon==False:
+    Game()
 
