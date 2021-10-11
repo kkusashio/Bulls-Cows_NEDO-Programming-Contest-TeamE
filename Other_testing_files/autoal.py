bfcff394b106e6c6b95fa2bfac00af9e936be993
kkusashio <kkusashio@gmail.com>
2021-10-11 22:04:01 +0900

    algo push

--- Other_testing_files/autoal.py
+++ Other_testing_files/autoal.py
@@ -3,15 +3,16 @@
 import random
 import math
 
-digits=5 #桁数
-numbers=16 #選択肢
-tries = 0 #トライ数
+digits=4 #桁数
+numbers=10 #選択肢
+number_of_try = 0 #トライ数
 hits = [] 
 blows = []
 guessed_numbers = [] #過去に予想したもの
 total_possibilities = math.factorial(numbers)/math.factorial(numbers-digits) #可能性の総数
 ans=[] #正解
 guess=[] #そのトライでの予測
+done = []
 """
 def get_HB():
     HB=player1.game_prepare._get_history #[[1,[12345,1,0]],[2,[adf23,0,3]],[3,[...]]] 3次元配列
@@ -35,8 +36,8 @@ def HBidentify(answer,guess): #HBの計算
     return [hits,blows]
 
 def get_random(): #ランダムで桁数の数字を出力
-    guess = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']#16進数の場合
-    #guess = ['0','1','2','3','4','5','6','7','8','9']#10進数の場合
+    #guess = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']#16進数の場合
+    guess = ['0','1','2','3','4','5','6','7','8','9']#10進数の場合
     random.shuffle(guess)
     guess = guess[:digits]
     return guess
@@ -47,10 +48,47 @@ def gen_answer():#正解のリストを生成
     return ans
 
 def detect_algorithm():#実際のアルゴリズム
+    tries=number_of_try
+    print("tries: ",tries)
+    while(len(done) != total_possibilities):
+        while(True):
+            guess=get_random()
+            if(guess not in done):
+                done.append(guess)
+                break
+        if(tries>2):
+            for j in range(tries-1):
+                h,b = HBidentify(guess,guessed_numbers[j])
+                if(h != hits[j] or b != blows[j]):#もう同じ組み合わせがあるかどうか
+                    break
+            else:
+                guessed_numbers.append(guess)
+                break
+        else:#chance=0
+            guessed_numbers.append(guess)
+            break
+    else:
+        print("error")
+    print("checked",len(done))
+    print("guess: ",guess)
+    while(True):
+        h,b=HBidentify(ans,guess)
+        print(h,"H",b,"B")
+        #h = (int(input("Hits: ")))
+        #b = (int(input("Blows: ")))
+        if(h + b <= digits):
+            hits.append(h)
+            blows.append(b)
+            print()
+            break
+    if(h == digits):
+        print("finnish",tries)
+
+def detect():#実際のアルゴリズム
     tries=0
     while(True):
         tries += 1
-        #print("tries: ",tries)
+        print("tries: ",tries)
         done = []
         while(len(done) != total_possibilities):
             while(True):
@@ -76,7 +114,7 @@ def detect_algorithm():#実際のアルゴリズム
         print("guess: ",guess)
         while(True):
             h,b=HBidentify(ans,guess)
-            #print(h,"H",b,"B")
+            print(h,"H",b,"B")
             #h = (int(input("Hits: ")))
             #b = (int(input("Blows: ")))
             if(h + b <= digits):
@@ -89,5 +127,9 @@ def detect_algorithm():#実際のアルゴリズム
             print("finnish",tries)
             break
 
-gen_answer()
-detect_algorithm()
+cont = 0
+ans = gen_answer()
+while (cont==0):
+    cont = int(input("continue press 0: "))
+    number_of_try += 1
+    detect_algorithm()
